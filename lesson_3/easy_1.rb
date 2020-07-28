bfb3559e69934e51a4957aa8de369e0302a44e83
Alexander Wilson <apwilson97@gmail.com>
2020-07-28 21:32:20 +0900

    Completed easy 1 exercises

---
+++ lesson_3/easy_1.rb
@@ -0,0 +1,42 @@
+# Question 1 
+
+# Expected output:
+# 1
+# 2
+# 2
+# 3
+
+# Question 2
+
+# 1) != means both operands are not equivalent and should be used in a conditional
+# 2) ! before something means that you return the opposite of the operands boolean value
+# 3) ! after something can mean that the method is mutating, but this depends on how method is implemented
+# 4) ? before something means that it is a ternary operator
+# 5) ? after something is usually attached to a method call but it depends on how that method is implemented 
+# 6) !! before something would turn that something into its boolean value
+
+# Question 3
+
+# advice[/important/] = 'urgent'
+
+# Question 4
+
+# delete_at(1) will delete the element at the array index of 1  [1,3,4,5]
+# delete(1) will just delete elements that contain the integer 1  [2,3,4,5]
+
+# Question 5
+
+# (10..100).cover?(42)
+
+# Question 6
+
+# 1) famous_words.prepend("Four score and ")
+# 2) famous_words.insert(0, "Four score and ")
+
+# Question 7
+
+# flintstones.flatten!
+
+# Question 8
+
+# flintstones.assoc("Barney")
