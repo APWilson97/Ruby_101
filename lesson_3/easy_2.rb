1002eafbcaa3e17e82b842acee8e83baa7f7d0ba
Alexander Wilson <apwilson97@gmail.com>
2020-07-30 21:19:52 +0900

    Completed easy 2 exercises

---
+++ lesson_3/easy_2.rb
@@ -0,0 +1,49 @@
+# Question 1
+
+ages.include?('Spot')
+
+ages.has_key?('Spot')
+
+ages.member?('Spot')
+
+# Question 2
+
+munsters_description.swapcase!
+
+munsters_description.capitalize!
+
+munsters_description.downcase!
+
+munsters_description.upcase!
+
+# Question 3
+
+ages.merge!(additional_ages)
+
+# Question 4
+
+advice.match?("Dino")
+
+# Question 5
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# Question 6
+
+flintstones.push("Dino")
+
+# Question 7
+
+flintstones.push("Dino", "Hoppy")
+
+# Question 8
+
+advice.slice!(0..38)
+
+# Question 9
+
+flintstones.count('t')
+
+# Question 10
+
+title.center(40)
