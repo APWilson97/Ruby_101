5ee4d2f4c8e325b974a979a13d72c3e45ddd819c
Alexander Wilson <apwilson97@gmail.com>
2020-08-07 22:04:42 +0900

    Completed exercise 1 of documentation_again

---
+++ rb_101_small_problems/documentation_again/exercise_1.rb
@@ -0,0 +1,7 @@
+# File::path (class method) returns string representation of file path (returns a string)
+# Called on class File
+
+# File#path (instance method) returns the path that was used to create the file the method is called upon as a string
+# Called on object of class File
+
+# Class methods are called on classes whereas instance methods are called on objects
