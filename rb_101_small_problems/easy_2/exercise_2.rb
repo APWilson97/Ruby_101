cac30374757bae316591f74047ae068aad285e7b
Alexander Wilson <apwilson97@gmail.com>
2020-08-20 21:00:33 +0900

    Completed exercise 2 of easy_2

---
+++ rb_101_small_problems/easy_2/exercise_2.rb
@@ -0,0 +1,12 @@
+puts 'Enter the length of the room in meters:'
+length = gets.chomp
+
+puts 'Enter the width of the room in meters:'
+width = gets.chomp
+
+feet_conversion = 10.7639
+
+area_meters = length.to_i * width.to_i
+area_feet = area_meters * feet_conversion
+
+puts "The area of the room is #{area_meters} square meters (#{area_feet} square feet)"
