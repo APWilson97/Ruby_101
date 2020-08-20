6a29efe3dc5aabaaf73a5346f73091c35c0083c5
Alexander Wilson <apwilson97@gmail.com>
2020-08-20 21:47:46 +0900

    Completed exercise 4 of easy_2

---
+++ rb_101_small_problems/easy_2/exercise_4.rb
@@ -0,0 +1,13 @@
+current_year = Time.now.year
+
+puts 'What is your age?'
+age = gets.chomp.to_i
+
+puts 'At what age would you like to retire?'
+retirement_age = gets.chomp.to_i
+
+years_till_retirement = retirement_age - age
+retirement_year = current_year + years_till_retirement
+
+puts "It's #{current_year}. You will retire in #{retirement_year}."
+puts "You have only #{years_till_retirement} years of work to go!"
