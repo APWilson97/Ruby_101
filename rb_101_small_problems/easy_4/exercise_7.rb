9ba9d437c25c645aee20c8788fa29266bc5a0056
Alexander Wilson <apwilson97@gmail.com>
2020-08-27 21:53:33 +0900

    Completed exercse 7 of easy_4

---
+++ rb_101_small_problems/easy_4/exercise_7.rb
@@ -0,0 +1,24 @@
+# method convert string of digits into appropriate number as integer
+# can't use conversion methods
+# all characters are numeric
+# input: string
+# output: integer
+
+DIGIT_LIST = {
+  '0' => 1,
+  '1' => 2,
+  '3' => 3,
+  '4' => 4,
+  '5' => 5,
+  '6' => 6,
+  '7' => 7,
+  '8' => 8,
+  '9' => 9
+}
+
+def string_to_integer(string)
+  integer = string.each_char { |char| DIGIT_LIST[char]}
+end
+
+puts string_to_integer('4321')
+puts string_to_integer('570')
